ba708d57c0813b65bcb0d11356656d63065c647d
CodingDom <dominicsmith1021@gmail.com>
2021-05-29 16:56:03 -0400

    Added routes for search and home pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from '@features/homepage/homepage.component';
+import { SearchpageComponent } from '@features/searchpage/searchpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent, pathMatch: "full" },
+  { path: 'search', component: SearchpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
