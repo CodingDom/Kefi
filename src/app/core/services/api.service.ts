823075ef0bb7f4e7e73be4bdd2b2d3ceb33674a7
CodingDom <dominicsmith1021@gmail.com>
2021-06-02 01:19:11 -0400

    Set up front-end api service

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private API_BASE_URL = "http://localhost:3000/api";
 
   constructor() { }
+
+  public getLocations(location: string) {
+    return axios.get(`${this.API_BASE_URL}/locations?location=${location}`);
+  }
+
+  public getProperties(cityId: number) {
+    return axios.get(`${this.API_BASE_URL}/properties?cityId=${cityId}`);
+  }
+
+  public getVrboPropertyDetails(propertyId: string) {
+    return axios.get(`${this.API_BASE_URL}/vrbo/${propertyId}`);
+  }
 }
