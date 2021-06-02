67e6962fe730c3ba5954196e70bfb2142ca62c37
CodingDom <dominicsmith1021@gmail.com>
2021-06-01 22:12:02 -0400

    Set up properties api

--- apiRoutes/airdna.js
+++ apiRoutes/airdna.js
@@ -12,5 +12,13 @@ module.exports = function(router) {
             });
     });
 
-
+    router.get('/properties', function(req, res) {
+        axios.get(`https://api.airdna.co/v1/market/property_list?access_token=${token}&city_id=${req.query.cityId}&start_month=5&start_year=2021&number_of_months=2&currency=native`)
+            .then(function(resp) {
+                res.send(resp.data);
+            })
+            .catch(function(err) {
+                res.status(500).send(err.message);
+            });
+    });
 }
