d6d49fb78e9f31963736a5902c7d80f429a62321
CodingDom <dominicsmith1021@gmail.com>
2021-06-01 21:47:13 -0400

    Setting up backend

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const path = require('path');
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Serve up static assets (usually on heroku)
+const distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+app.listen(PORT, function() {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
+});
+
+module.exports = app;
