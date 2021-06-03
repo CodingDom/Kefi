abf22cae1a773a7472194bf6b99d3932a2666348
CodingDom <dominicsmith1021@gmail.com>
2021-06-03 00:50:45 -0400

    Changed api scrape endpoint

--- apiRoutes/vrbo.js
+++ apiRoutes/vrbo.js
@@ -4,28 +4,20 @@ const cheerio = require('cheerio');
 module.exports = function(router) {
     router.get('/vrbo/:id', function(req, res) {
         const id = req.params.id;
-        const url = `http://api.allorigins.win/get?url=https%3A//www.vrbo.com/${id}&callback=?`;
-        axios.get(url, {
-                headers: {
-                    "Content-Type": "text/json"
-                }
-            })
+        const url = `http://www.vrbo.com/${id}`;
+        axios.get(url)
             .then(function(resp) {
                 const cleanedHtml = "<fake>" + resp.data + "</fake>";
                 const $ = cheerio.load(cleanedHtml);
                 const dataScript = $("script:contains('window.__INITIAL_STATE__ =')").html();
                 const startIndex = dataScript.indexOf("{");
-                const endIndex = dataScript.indexOf(";");
-                const dataObj = dataScript.substring(startIndex, endIndex)
-                    .replace(/{\\"/g, `{"`).replace(/\\"}/g, `"}`)
-                    .replace(/\\":/g, `":`).replace(/:\\"/g, `:"`)
-                    .replace(/,\\"/g, `,"`).replace(/\\",/g, `",`)
-                    .replace(/\[\\"/g, `["`).replace(/\\"\]/g, `"]`);
-                const uselessIndex = dataObj.indexOf(`,"abacusTestsReducer`);
-                const info = JSON.parse(dataObj.substring(0, uselessIndex) + "}");
+                const endIndex = dataScript.indexOf("};") + 1;
+                const dataObj = dataScript.substring(startIndex, endIndex);
+                const info = JSON.parse(dataObj);
                 res.send(info);
             })
             .catch(function(err) {
+                console.log(err);
                 res.status(500).send(err.message);
             });
     });
