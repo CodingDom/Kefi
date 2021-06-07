b97859ad2a4b90f1e76eb32150eeebf32529ab76
CodingDom <dominicsmith1021@gmail.com>
2021-06-07 11:43:29 -0400

    Split the travel news into separate requests to increase speed of request

--- apiRoutes/travelpulse.js
+++ apiRoutes/travelpulse.js
@@ -6,6 +6,51 @@ module.exports = function(router) {
         axios.get("https://travelpulse.com/destinations")
             .then(function(resp) {
                 const $ = cheerio.load(resp.data);
+
+                $(".regional_news li").each((index, elem) => {
+                    const locationLink = $(elem).find("> p").first().find("a");
+                    const locationName = locationLink.text();
+                    const locationUrl = locationLink.attr("href");
+                    const thumbnail = $(elem).find("> a").find("img");
+                    const thumbnailAlt = thumbnail.attr("alt");
+                    const thumbnailSrc = thumbnail.data("src");
+                    const titleLink = $(elem).find("> a").next().find("a");
+                    const titleUrl = titleLink.attr("href");
+                    const titleText = titleLink.text();
+                    const categoryLink = $(elem).find("> p").last().find("a");
+                    const categoryText = categoryLink.text();
+                    const categoryUrl = categoryLink.attr("href");
+
+                    destinations.push({
+                        location: {
+                            url: locationUrl,
+                            title: locationName
+                        },
+                        thumbnail: {
+                            alt: thumbnailAlt,
+                            src: thumbnailSrc
+                        },
+                        category: {
+                            url: categoryUrl,
+                            title: categoryText
+                        },
+                        headline: titleText,
+                        url: titleUrl
+                    });
+                });
+
+                res.send(destinations);
+            })
+            .catch(function(err) {
+                console.log(err);
+                res.status(500).send(err.message);
+            });
+    });
+
+    router.get('/travel-news/all', function(req, res) {
+        axios.get("https://travelpulse.com/destinations")
+            .then(function(resp) {
+                const $ = cheerio.load(resp.data);
                 const categoryRequests = [];
                 $(".categories").find("a").each((index, elem) => {
                     const href = $(elem).attr("href");
