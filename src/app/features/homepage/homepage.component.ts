5880e1a3f945339b594cee99a0d11d7e2bad8a63
CodingDom <dominicsmith1021@gmail.com>
2021-06-07 13:56:33 -0400

    Added browse properties button

--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TravelNews } from '@core/interfaces/travel-news';
 import { ApiService } from '@core/services/api.service';
 
@@ -12,32 +13,14 @@ export class HomepageComponent implements OnInit {
   locationSearch: any;
 
   constructor(
-    private api : ApiService
+    private api : ApiService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.api.getTravelNews().then(data => {
       this.shuffleArray(data);
       this.destinations = data.slice(0, 6);
-      const locations = {};
-      let currIndex = 0;
-      for (let i = 0; i < this.destinations.length; i++) {
-        this.api.getLocations(this.destinations[i].location.title)
-        .then(data => {          
-          const location = data[Math.abs(Math.random() * data.length)];
-          locations[this.destinations[i].location.title] = `/search?location=${encodeURIComponent(location.name)}&locationId=${location.city.id}`;          
-        })
-        .catch(err => {
-          
-        })
-        .finally(() => {
-          currIndex++;
-          if (currIndex == this.destinations.length) {
-            this.locationSearch = locations;
-          }
-          console.log(currIndex);
-        });
-      }      
     });
   }
 
@@ -46,6 +29,21 @@ export class HomepageComponent implements OnInit {
           const j = Math.floor(Math.random() * (i + 1));
           [array[i], array[j]] = [array[j], array[i]];
       }
+  }
+
+  searchByLocation(name: string) {
+    this.api.getLocations(name)
+    .then(({items}) => {          
+      const location = items[Math.floor(Math.random() * items.length)];  
+      const query = {
+        location: location.name,
+        locationId: location.city.id
+      };          
+      this.router.navigate(['/search'], { queryParams: query });
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 
 }
